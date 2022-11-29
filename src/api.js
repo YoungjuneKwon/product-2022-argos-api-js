69a33989e861cfa61398683859f4ea40188e2d70
YoungjuneKwon <yjkwon@winm2m.com>
2022-11-29 15:30:42 +0900

    0.1.0

---
+++ src/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+const notionDataConverter = r => ({
+  id: r.id,
+  createdAt: r.created_time,
+  updatedAt: r.last_edited_time,
+  entity: r.properties.entity.title[0].plain_text,
+  body: JSON.parse(r.properties.body.rich_text[0].plain_text),
+  owner: r.properties.owner.relation[0].id
+})
+
+class API {
+  constructor(host) {
+    this.prefix = host
+    this.headers = {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  async get(path) {
+    return (await axios.get(`${this.prefix}${path}`, { headers: this.headers })).data
+  }
+
+  async post(path, data) {
+    return (await axios.post(`${this.prefix}${path}`, data, { headers: this.headers })).data
+  }
+
+  async login({email, password}) {
+    const response = await this.post('/login', { email, password })
+    if (response.token) this.headers['Authorization'] = response.token
+    return response
+  }
+
+  async find({entity, contains}) {
+    const response = await this.post(`/find`, { entity, contains })
+    return response.map(notionDataConverter)
+  }
+
+  async get(id) {
+    const response = await this.post('/get', { id })
+    return notionDataConverter(response)
+  }
+
+  delete(id) {
+    return this.post('/delete', { id })
+  }
+
+  async upsert({entity, body, id}) {
+    const response = await this.post('/upsert', { entity, body: JSON.stringify(body), id })
+    return notionDataConverter(response)
+  }
+}
+
+export default API
---
+++ test/main.test.js
@@ -0,0 +1,18 @@
+import API from '../index'
+import config from './test.config'
+
+const ctx = {}
+
+test("basic CRUD", async () => {
+  var res = {}
+  ctx.api = new API(config.prefix)
+  res = await ctx.api.login(config.sampleUser)
+  expect(res.token).toBeTruthy()
+  res = await ctx.api.upsert({ entity: 'sample', body: {hello: 'world'}})
+  expect(res.id).toBeTruthy()
+  res.body.hello = 'world2'
+  res = await ctx.api.upsert(res)
+  expect(res.body.hello).toBe('world2')
+  res = await ctx.api.delete(res.id)
+  expect(res.id).toBeTruthy()
+})
